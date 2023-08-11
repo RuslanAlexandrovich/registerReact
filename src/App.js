9391c289f0de334b3c39605890e2eecd1d47f1cf
Ruslan Alexandrovich <Motuzkinra@gmail.com>
2023-08-11 18:20:28 +0300

    react 	modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	modified:   src/App.css 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
 import logo from './logo.svg';
 import './App.css';
+import Form  from 'react-bootstrap/Form';
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from 'react';
+import {Container, Row, Col} from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: {errors}
+  } = useForm();
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const onSumbit = data=>{
+    console.log(data);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row>
+          <Col className='wrapper'>
+            <Form onSubmit={handleSubmit(onSumbit)} className="form_reg">
+            <h2 className="title_form mb-5">Реєстрація</h2>
+            <Form.Group className="mb-3" controlId="formBasicName">
+              <Form.Label className='App-label'>Ім'я *</Form.Label>
+              <Form.Control type="text" placeholder="" {...register("name", {required: true,
+                    pattern: /^[A-ZА-ЯІ][a-zа-яі]{1,}$/u})} />
+              <Form.Text className="text-muted" >
+              Ім'я має містити мінімум дві літери, з першою великою і рештою малих літер.
+              </Form.Text>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicSurname">
+              <Form.Label className='App-label'>Прізвище *</Form.Label>
+              <Form.Control type="text" placeholder="" {...register("surName", {required: true,
+                    pattern: /^[A-ZА-ЯІ][a-zа-яі]{1,}$/u})}/>
+              <Form.Text className="text-muted" >
+              Прізвище має містити мінімум дві літери, з першою великою і рештою малих літер.
+              </Form.Text>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label className='App-label'>Пошта *</Form.Label>
+              <Form.Control type="email" placeholder="" {...register("email", {required: true,
+                    pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/i})} />
+              <Form.Text className="text-muted" >
+              Приклад: Email@email.ua
+              </Form.Text>
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label className='App-label'>Номер телефону</Form.Label>
+              <Form.Control type="text" id="phone" placeholder="+380 (необов'язково)" {...register("phone", {pattern: /^$|^\+380\d{9}$/})}/>
+              {errors.phone && (
+                  <Form.Text className="text-danger">
+                    Будь ласка, введіть номер телефону у форматі +380 і 9 цифр вашого телефону.
+                  </Form.Text>
+                )}
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Пароль</Form.Label>
+              <Form.Control type={showPassword ? "text" : "password"} id="password" placeholder="Пароль має містити не менше 6 символів, 1 велику літеру, 1 цифру." {...register("password", {required: true,
+                    pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/})} />
+              {errors.password && (
+                  <Form.Text className="text-danger">
+                    Пароль має містити не менше 6 символів, 1 велику літеру, 1 цифру, і символи латинського алфавіту.
+                  </Form.Text>
+                )}
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox"  id="showPass"  label="Видимий пароль" onChange={() => setShowPassword(!showPassword)} />
+            </Form.Group>
+            <Button type="submit" id="submit">
+            Зареєструвати
+            </Button>
+                </Form>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
